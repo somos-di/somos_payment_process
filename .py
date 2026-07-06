080762010ba277d76e019d2d8be3e143b59b97f0
Luduvico <galahadempr@gmail.com>
2026-07-06 08:37:40 -0300

    Fixed the var name

--- .py
+++
@@ -1,9 +0,0 @@
-# max consecutive difference in a list
-def find_max_difference(lst):
-    max_diff = 0
-    for i in range(len(lst) - 1):
-
-
-        if diff > max_diff:
-            max_diff = diff
-    return max_diff
